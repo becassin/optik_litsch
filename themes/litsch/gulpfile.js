3e11212b9d3af6a497a8abeb3e84bd5223b75bdb
Guillaume <bonjour@giom.fr>
2016-10-07 16:21:26 +0200

    gulp setup

---
+++ themes/litsch/gulpfile.js
@@ -0,0 +1,64 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
+var sourcemaps = require('gulp-sourcemaps');
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('sass/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('js'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('js'));
+});
+
+gulp.task('js-watch', ['lint', 'scripts'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('sass-watch', ['sass'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('serve', function () {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        proxy: "litsch.dev"
+    });
+
+    // add browserSync.reload to the tasks array to make
+    // all browsers reload after tasks are complete.
+    gulp.watch("js/*.js", ['js-watch']);
+    gulp.watch("sass/*.scss", ['sass-watch']);
+
+});
+
+// Default Task
+gulp.task('default', ['lint', 'sass', 'scripts', 'serve']);
